36ca31cf4fe77bdd8da9556f2ef126f643d62c50
CorentinGlaus <glaus.corentin@gmail.com>
2022-04-11 19:58:17 -0400

    navigation works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExtensionsComponent } from './extensions/extensions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', component: ExtensionsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
